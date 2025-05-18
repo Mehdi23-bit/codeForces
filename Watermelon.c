5828f73ab74d260fc6d9ad8b72f8a28cb6bae7ff
mehdi <elmehdiiskandar3@gmail.com>
2025-05-18 01:46:58 +0100

    fix a bug in watermelon

--- Watermelon.c
+++ Watermelon.c
@@ -1,28 +1,19 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-bool
-isEven(int n){
-    if(n%2==0){
-        return true;
-    }
-    return false;
-} 
-bool
-watermelon(int n){
-    int i=1;
-    while(i<n){
-        if(isEven(i) && isEven(n-i))
-        return true;
-        i++;
 
+char*
+watermelon(int n){
+    if(n%2==0 && n>2){
+        return "YES";
     }
-    return false;
+    
+    return "NO";
 }
 
 int main(){
 int a;
 scanf("%d",&a);
-printf("%d",watermelon(a));
+printf("%s",watermelon(a));
 return 0;
 }
